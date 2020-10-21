da0e5e8de708ec66766803070112433534d66711
develop <you@example.com>
2020-10-21 21:29:06 +0300

    Динамическая подгрузка пицц

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import { Cart, Home } from './pages';
 
 function App() {
 
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      })
+  }, []);
+
   return (
     <div className="wrapper">
 
@@ -13,7 +23,7 @@ function App() {
 
       <div className="content">
 
-        <Route path="/" component={Home} exact />
+        <Route path="/" render={() => <Home items={pizzas} />} exact />
         <Route path="/cart" component={Cart} exact />
 
       </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Categories from './Categories';
 import Header from './Header';
 import Button from './Button';
 import SortPopup from './SortPopup';
+import PizzaBlock from './PizzaBlock';
 
-export { Categories, Header, Button, SortPopup }
+export { Categories, Header, Button, SortPopup, PizzaBlock }
