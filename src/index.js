6812bea802a7c9fb8c64c70ec551fc393c388215
AlexeyKokoryshkin <7f@mail.ru>
2020-11-03 19:59:54 +0300

    connect redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createStore } from 'redux';
 
 import './scss/app.scss';
 
 import App from './App';
+
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+};
+
+const store = createStore(counter);
+
+console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
