033a2ace263dc48012b3fe36e5f18eb71f54ca43
akokoryshkin <akokoryshkin@prosv.ru>
2020-11-04 19:11:18 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import './scss/app.scss';
-
-import App from './App';
-
+import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import App from './App';
+import './scss/app.scss';
 
-let inc = () => store.dispatch({ type: 'add' })
-
-store.subscribe(() => console.log("Изменился store", store.getState()))
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
-      <button onClick={inc}> + 1 </button>
+      <Provider store={store}>
+         <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
